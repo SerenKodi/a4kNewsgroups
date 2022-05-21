15b835026ac8c6dfb33d85bebd1e48a01c9d2980
drinfernoo <drinfernoo@yahoo.com>
2022-05-21 14:14:59 -0700

    :bug: - Possible fixes for imports and other errors

--- providers/a4kNewsgroups/en/adaptive/__init__.py
+++ providers/a4kNewsgroups/en/adaptive/__init__.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, unicode_literals
+
 from providerModules.a4kNewsgroups import common
 
 __all__ = common.get_all_relative_py_files(__file__)
